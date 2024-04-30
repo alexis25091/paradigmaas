4837ad75174d932f0835fd1d6f0b6bc7d7d5a0ac
alexis25091 <152936639+alexis25091@users.noreply.github.com>
2024-04-29 21:51:13 -0600

    Update circulo.java

--- circulo.java
+++ circulo.java
@@ -11,6 +11,10 @@ public class circulo {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public double getArea() {
         return calcularArea();
     }
@@ -19,11 +23,30 @@ public class circulo {
         return calcularPerimetro();
     }
 
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
     private double calcularArea() {
         return Math.PI * Math.pow(radio, 2);
     }
 
     private double calcularPerimetro() {
         return 2 * Math.PI * radio;
+    }
+
+    public static void main(String[] args) {
+        circulo miCirculo = new circulo("rojo", 5.0);
+        miCirculo.setColor("azul");
+        miCirculo.setRadio(7.0);
+        System.out.println("Información del círculo:");
+        System.out.println("Color: " + miCirculo.getColor());
+        System.out.println("Radio: " + miCirculo.getRadio());
+        System.out.println("Área: " + miCirculo.getArea());
+        System.out.println("Perímetro: " + miCirculo.getPerimetro());
     }
 }
