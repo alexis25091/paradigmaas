baa6f4ec78369d1940d0bbdbc9f8b9c3a6473e28
alexis25091 <152936639+alexis25091@users.noreply.github.com>
2024-04-29 21:51:44 -0600

    Update cuadrado.java

--- cuadrado.java
+++ cuadrado.java
@@ -11,6 +11,10 @@ public class cuadrado {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public double getArea() {
         return calcularArea();
     }
@@ -19,11 +23,31 @@ public class cuadrado {
         return calcularPerimetro();
     }
 
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
     private double calcularArea() {
         return lado * lado;
     }
 
     private double calcularPerimetro() {
         return 4 * lado;
+    }
+
+    public static void main(String[] args) {
+        // Crear un cuadrado
+        cuadrado miCuadrado = new cuadrado("verde", 5.0);
+        miCuadrado.setColor("amarillo");
+        miCuadrado.setLado(7.0);
+        System.out.println("Información del cuadrado:");
+        System.out.println("Color: " + miCuadrado.getColor());
+        System.out.println("Lado: " + miCuadrado.getLado());
+        System.out.println("Área: " + miCuadrado.getArea());
+        System.out.println("Perímetro: " + miCuadrado.getPerimetro());
     }
 }
